902dac16fed5561ae575809fd76ba3aaf36e2d27
Chris Bushell <cbushell@CB8047.local>
2012-06-14 12:16:00 -0700

    adding .bash* files and simple setup script

---
+++ .bashrc
@@ -0,0 +1,3 @@
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+export PS1="\w$"
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bash_login ~/.bash_login
