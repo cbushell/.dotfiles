3277ca2a9d0fb6e3d13b3c94a1c01083ac3f4780
Chris Bushell <c.bushell@gmail.com>
2012-06-15 17:14:23 -0700

    adding .vimrc

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
