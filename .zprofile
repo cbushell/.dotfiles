39c3a30fc7582c49e069220a8dc556aa93347f6a
Chris Bushell <cbushell@Chriss-MacBook-Pro.local>
2021-08-20 17:01:40 +0100

    complete hombew setup

--- .zprofile
+++ .zprofile
@@ -2,3 +2,4 @@ ZSH_THEME=robbyrussell
 
 source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.zprofile ~/.zprofile
-#ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
